b2dd8b8b771a3e35eb18ff914bbc569176fc8bc8
Alexandre Calcio Gaudino <alexandre.calciogaudino@gmail.com>
2024-04-01 20:36:21 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Alexandre Calcio Gaudino on 01/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

0d8ab13ab108de83480a0b62125ccc2d4e0a6daa
alx-calcio <alexandre.calciogaudino@gmail.com>
2024-04-09 22:07:15 +0200

    Finishing the game

--- Memorize/EmojiMemorizeGame.swift
+++ Memorize/EmojiMemorizeGame.swift
@@ -5,4 +5,34 @@
 //  Created by Alexandre Calcio Gaudino on 06/04/2024.
 //
 
-import Foundation
+import SwiftUI
+
+
+
+class EmojiMemorizeGame: ObservableObject{
+    private static let emojis = ["😱", "😨", "😰", "😥", "😓", "😐", "🫤", "😑", "😬", "😳", "😢", "🤯"]
+    
+    private static func createMemorizeGame() -> MemorizeGame<String>{
+     return MemorizeGame<String>(numberOfPairsOfCards: 10){pairIndex in
+         if emojis.indices.contains(pairIndex){
+        return emojis[pairIndex]
+         } else {
+             return "😬"
+         }}}
+    
+    @Published private var model = createMemorizeGame()
+    
+    var cards: Array<MemorizeGame<String>.Card>{
+        return model.cards
+    }
+    
+    // MARK: - Intents
+    
+    func shuffle(){
+        model.shuffle()
+    }
+    
+    func choose(_ card: MemorizeGame<String>.Card){
+        model.choose(card)
+    }
+}
--- Memorize/EmojiMemorizeGameView.swift
+++ Memorize/EmojiMemorizeGameView.swift
@@ -8,41 +8,58 @@
 import SwiftUI
 
 struct EmojiMemorizeGameView: View {
-    var viewModel: EmojiMemorizeGame
+    @ObservedObject var viewModel: EmojiMemorizeGame
     let emojis : [String] = ["😱", "😨", "😰", "😥", "😓", "😐", "🫤", "😑"]
     var body: some View {
+        VStack{
             ScrollView(showsIndicators: false
-            ){cards}
-        .foregroundColor(.accentColor)
-        .padding()
+            ){
+                cards.animation(.default, value: viewModel.cards)
+            }
+                .foregroundColor(.accentColor)
+                .padding()
+            Button(action: {viewModel.shuffle()}){
+                Image(systemName: "shuffle.circle.fill").font(.largeTitle)
+            }
+        }
     }
     var cards: some View{
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 150, maximum: 200))]) {
-            ForEach(emojis.indices, id: \.self){index in
-                CardView(content: emojis[index]).aspectRatio(2/3, contentMode: .fit)
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0) {
+            ForEach(viewModel.cards){card in
+                CardView(card)
+                    .aspectRatio(2/3, contentMode: .fit)
+                    .padding(4)
+                    .onTapGesture {
+                        viewModel.choose(card)
+                    }
             }
         }
     }
 }
 
 struct CardView: View{
-    let content: String
-    @State var isFaceUp: Bool = false
+    let card: MemorizeGame<String>.Card
+    
+    init(_ card: MemorizeGame<String>.Card) {
+        self.card = card
+    }
+    
     var body: some View{
         ZStack{
             let base = RoundedRectangle(cornerRadius: 20)
             Group {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 4)
-                Text(content).font(.largeTitle)}
-            .opacity(isFaceUp ? 1 : 0 )
-            base.fill().opacity(isFaceUp ? 0 :1 )
-        }.onTapGesture {
-            isFaceUp.toggle()
-        }
+                Text(card.content)
+                    .font(.system(size: 200))
+                    .minimumScaleFactor(0.01)
+                .aspectRatio(1, contentMode: .fit)}
+            .opacity(card.isFaceUp ? 1 : 0 )
+            base.fill().opacity(card.isFaceUp ? 0 : 1 )
+        }.opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
     }
 }
 
 #Preview {
-    EmojiMemorizeGameView()
+    EmojiMemorizeGameView(viewModel: EmojiMemorizeGame())
 }

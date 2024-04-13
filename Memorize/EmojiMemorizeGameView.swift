ee95af836fefe5a6486bbaa7e5eeb1227bb6c1ee
alx-calcio <alexandre.calciogaudino@gmail.com>
2024-04-13 14:01:46 +0200

    Added autoresizing to fit

--- Memorize/EmojiMemorizeGameView.swift
+++ Memorize/EmojiMemorizeGameView.swift
@@ -10,35 +10,61 @@ import SwiftUI
 struct EmojiMemorizeGameView: View {
     @ObservedObject var viewModel: EmojiMemorizeGame
     let emojis : [String] = ["😱", "😨", "😰", "😥", "😓", "😐", "🫤", "😑"]
+    private let cardAspectRatio: CGFloat = 2/3
     var body: some View {
         VStack{
-            ScrollView(showsIndicators: false
-            ){
-                cards.animation(.default, value: viewModel.cards)
-            }
-                .foregroundColor(.accentColor)
+            cards.animation(Animation.smooth, value: viewModel.cards)
+            .foregroundColor(Color.accentColor)
                 .padding()
             Button(action: {viewModel.shuffle()}){
                 Image(systemName: "shuffle.circle.fill").font(.largeTitle)
             }
         }
     }
-    var cards: some View{
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0) {
-            ForEach(viewModel.cards){card in
-                CardView(card)
-                    .aspectRatio(2/3, contentMode: .fit)
-                    .padding(4)
-                    .onTapGesture {
-                        viewModel.choose(card)
-                    }
+    
+    @ViewBuilder
+    private var cards: some View {
+        GeometryReader { geometry in
+            let gridItemSize = gridItemWidthThatFits(
+                count: viewModel.cards.count,
+                size: geometry.size,
+                atAspectRation: cardAspectRatio)
+            
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: gridItemSize), spacing: 0)], spacing: 0) {
+                ForEach(viewModel.cards){card in
+                    CardView(card)
+                        .aspectRatio(cardAspectRatio, contentMode: .fit)
+                        .padding(4)
+                        .onTapGesture {
+                            viewModel.choose(card)
+                        }
+                }
             }
         }
+    }
+    
+    private func gridItemWidthThatFits (
+        count: Int,
+        size: CGSize,
+        atAspectRation aspectRatio: CGFloat) -> CGFloat
+    {
+        let count = CGFloat(count)
+        var columnCount = 1.0
+        repeat {
+            let width = size.width / columnCount
+            let height = width / aspectRatio
+            let rowCount = (count / columnCount).rounded(.up)
+            if rowCount * height < size.height {
+                return (size.width / columnCount).rounded(.down)
+            }
+            columnCount += 1
+        } while columnCount < count
+        return min(size.width / count, size.height * aspectRatio).rounded(.down)
     }
 }
 
 struct CardView: View{
-    let card: MemorizeGame<String>.Card
+    private let card: MemorizeGame<String>.Card
     
     init(_ card: MemorizeGame<String>.Card) {
         self.card = card

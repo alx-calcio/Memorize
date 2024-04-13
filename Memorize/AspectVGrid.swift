0c4fb68abc43fcf12f2351a7bc4a318c1823fb51
alx-calcio <alexandre.calciogaudino@gmail.com>
2024-04-13 21:10:24 +0200

    added viewbuilder

---
+++ Memorize/AspectVGrid.swift
@@ -0,0 +1,18 @@
+//
+//  AspectVGrid.swift
+//  Memorize
+//
+//  Created by Alexandre Calcio Gaudino on 13/04/2024.
+//
+
+import SwiftUI
+
+struct AspectVGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AspectVGrid()
+}

55136becff3664ad51426668ec9faac7f42d1075
alx-calcio <alexandre.calciogaudino@gmail.com>
2024-04-13 22:11:55 +0200

    Creating CardView separate file

---
+++ Memorize/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  Memorize
+//
+//  Created by Alexandre Calcio Gaudino on 13/04/2024.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
